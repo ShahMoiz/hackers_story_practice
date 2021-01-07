414502eda6a6d6d238bf1dc597d14b2c76731afc
Abdul Moiz <abdul_moiz10@hotmail.com>
2021-01-07 16:31:58 +0500

    New Component created and props add

--- src/App.js
+++ src/App.js
@@ -3,15 +3,48 @@ import './App.css';
 
 
 const App = () => {
-    <div >
+  const handleChange = event => {
+    console.log(event.target.value)
+  }
+
+  const stories = [
+    {
+        title: 'React',
+        url: 'https://google.com',
+        auther: 'Abdul Moiz',
+        num_comments: 3,
+        points: 4,
+        objectId: 0
+    },
+    {
+        title: 'Angular',
+        url: 'https://google.com',
+        auther: 'Abdul hadi',
+        num_comments: 3,
+        points: 4,
+        objectId: 1
+    },
+    {
+        title: 'Laravel',
+        url: 'https://google.com',
+        auther: 'Haris',
+        num_comments: 3,
+        points: 4,
+        objectId: 2
+    },
+];
+  return (
+  <div >
       <header >
         <h1>My Hacker Story</h1>
         <label htmlFor="search">Search:</label>
-        <input id="search" type="text"/>
+        <input id="search" type="text" onChange={handleChange}/>
       </header>
 
-      <List />
+      <List list={stories}/>
     </div>
+   
+)
 }
 
 export default App;
--- src/ListComponent/list.js
+++ src/ListComponent/list.js
@@ -1,40 +1,18 @@
-export const List = () => {
-    const list = [
-        {
-          title: 'React',
-          url: 'https://google.com',
-          auther: 'Abdul Moiz',
-          num_comments: 3,
-          points: 4,
-          objectId: 0
-        },
-        {
-          title: 'Angular',
-          url: 'https://google.com',
-          auther: 'Abdul hadi',
-          num_comments: 3,
-          points: 4,
-          objectId: 1
-        },
-        {
-          title: 'Laravel',
-          url: 'https://google.com',
-          auther: 'Haris',
-          num_comments: 3,
-          points: 4,
-          objectId: 2
-        },
-      ];
 
-        <div>
-            {list.map(list => {
-                <div key={list.objectId}>
-                <h2>Title: {list.title}</h2>
-                <a href={list.url}/>Url: {list.url}
-                <h4>Auther: {list.auther}</h4>
-                <p>Comments: {list.num_comments}</p>
-                <p>Points: {list.points}</p>
-                </div>
-          })}
+export const List = props => 
+(
+    <div>
+        {props.list.map(list =>
+        (
+        <div key={list.objectId}>
+            <h2>Title: {list.title}</h2>
+            <a href={list.url} />Url: {list.url}
+            <h4>Auther: {list.auther}</h4>
+            <p>Comments: {list.num_comments}</p>
+            <p>Points: {list.points}</p>
         </div>
-}
+        ))
+        }
+
+    </div>
+)
